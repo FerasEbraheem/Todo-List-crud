fd91732dfc88b7a2bb1def9f39a23190a67a2dec
ferasebraheem <feras.kh.ebraheem@gmail.com>
2025-04-04 04:01:52 +0000

    Create the TodoDB class with all its methods.

--- test-mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-try  {
-    $mysqli = new mysqli("localhost", "j23d", "beep", "test");
-    echo "Erfolgreich verbunden!";
-
-} catch (Exception $exc) {
-    die("Verbindung fehlgeschlagen: " . $exc);
-}
-?>
